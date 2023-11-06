d640f610b7716622e1a5ce8b2dcf229510b7e9c2
Jonas Götz <jonas.goetz01@web.de>
2023-11-06 10:56:22 +0100

    working solution with mandelbrot

--- src/fractol.c
+++ src/fractol.c
@@ -6,7 +6,7 @@
 /*   By: jgotz <jgotz@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 16:33:48 by jgotz             #+#    #+#             */
-/*   Updated: 2023/11/03 17:57:18 by jgotz            ###   ########.fr       */
+/*   Updated: 2023/11/06 10:55:56 by jgotz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,74 +15,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define WIDTH 360
-#define HEIGHT 360
-#define MAX_ITERATIONS 100
+#define WIDTH 500
+#define HEIGHT 500
+#define MAX_ITERATIONS 200
 #define LIMIT 16
 
-double	map(int value, int start1, int stop1, int start2, int stop2)
+double	map(double value, double start1, double stop1, double start2,
+		double stop2)
 {
 	return (start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1)));
 }
 
-uint32_t	rgb_color(int r, int g, int b, int a)
+int	get_rgba(int r, int g, int b, int a)
 {
-	return ((r << 24) | (g << 16) | (b << 8) | a);
+	return (r << 24 | g << 16 | b << 8 | a);
 }
 
-int	mandelbrot(double real, double imag)
+void	mandelbrot(void *param)
 {
-	int		n;
-	double	oreal;
-	double	oimag;
-	double	real_squared;
-	double	imag_squared;
+	int				color;
+	t_helper		helper;
+	t_mandelbrot	*mbt;
 
-	n = 0;
-	oreal = real;
-	oimag = imag;
-	while (n < MAX_ITERATIONS)
+	mbt = (t_mandelbrot *)param;
+	helper.x = 0;
+	helper.y = 0;
+	while (helper.y < HEIGHT)
 	{
-		real_squared = real * real;
-		imag_squared = imag * imag;
-		if (real_squared + imag_squared > LIMIT * LIMIT)
-			break ;
-		imag = 2 * real * imag + oimag;
-		real = real_squared - imag_squared + oreal;
-		n++;
+		helper.x = 0;
+		while (helper.x < WIDTH)
+		{
+			helper.a = map(helper.x, 0, WIDTH, -(*mbt->zoom), *mbt->zoom);
+			helper.b = map(helper.y, 0, HEIGHT, -(*mbt->zoom), *mbt->zoom);
+			helper.ca = helper.a;
+			helper.cb = helper.b;
+			helper.n = 0;
+			while (helper.n < MAX_ITERATIONS)
+			{
+				helper.aa = helper.a * helper.a - helper.b * helper.b;
+				helper.bb = 2 * helper.a * helper.b;
+				helper.a = helper.aa + helper.ca;
+				helper.b = helper.bb + helper.cb;
+				if (helper.a * helper.a + helper.b * helper.b > LIMIT)
+					break ;
+				helper.n++;
+			}
+			color = map(helper.n, 0, MAX_ITERATIONS, 0, 1);
+			color = map(sqrt(color), 0, 1, 0, 255);
+			mlx_put_pixel(mbt->img, helper.x, helper.y, get_rgba(color, color,
+					color, 255));
+			helper.x++;
+		}
+		helper.y++;
 	}
-	return (n);
+	mlx_image_to_window(mbt->mlx, mbt->img, 0, 0);
+}
+
+void	scroll(double a, double b, void *zoom)
+{
+	a++;
+	*(double *)zoom -= b * 0.01;
 }
 
 int	main(void)
 {
-	mlx_t		*mlx;
-	mlx_image_t	*img;
-	int			x;
-	int			y;
-	int			color;
-	double		real;
-	double		imag;
-	int			iterations;
+	t_mandelbrot	mbt;
+	mlx_t			*mlx;
+	mlx_image_t		*img;
+	double			zoom;
 
+	zoom = 2.5;
 	mlx = mlx_init(WIDTH, HEIGHT, "fract'ol", true);
 	img = mlx_new_image(mlx, WIDTH, HEIGHT);
-	y = 0;
-	while (y < HEIGHT)
-	{
-		x = 0;
-		while (x < WIDTH)
-		{
-			real = map(x, 0, WIDTH, -2.0, 2.0);
-			imag = map(y, 0, HEIGHT, -2.0, 2.0);
-			iterations = mandelbrot(real, imag);
-			color = map(iterations, 0, MAX_ITERATIONS, 0, 255);
-			mlx_put_pixel(img, x, y, rgb_color(255, color, color, color));
-			x++;
-		}
-		y++;
-	}
-	mlx_image_to_window(mlx, img, 0, 0);
+	mlx_scroll_hook(mlx, scroll, &zoom);
+	mbt.img = img;
+	mbt.mlx = mlx;
+	mbt.zoom = &zoom;
+	mlx_loop_hook(mlx, mandelbrot, &mbt);
 	mlx_loop(mlx);
 	return (0);
 }
--- src/fractol.h
+++ src/fractol.h
@@ -6,7 +6,7 @@
 /*   By: jgotz <jgotz@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 16:28:14 by jgotz             #+#    #+#             */
-/*   Updated: 2023/11/02 12:59:49 by jgotz            ###   ########.fr       */
+/*   Updated: 2023/11/06 10:54:41 by jgotz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,25 @@
 # include "../lib/MLX42/include/MLX42/MLX42.h"
 # include "../lib/libft/ft_printf.h"
 # include <math.h>
+
+typedef struct s_mandelbrot
+{
+	mlx_image_t	*img;
+	double		*zoom;
+	mlx_t		*mlx;
+}				t_mandelbrot;
+
+typedef struct s_helper
+{
+	double		a;
+	double		b;
+	double		ca;
+	double		cb;
+	double		aa;
+	double		bb;
+	int			n;
+	int			x;
+	int			y;
+}				t_helper;
 
 #endif
