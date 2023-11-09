5d5e25cafe24d7ace17e752dc0cd03278c3c20d1
Jonas Götz <jonas.goetz01@web.de>
2023-11-09 16:17:11 +0100

    moved resized hook and command like args

--- src/fractol.c
+++ src/fractol.c
@@ -6,7 +6,7 @@
 /*   By: jgotz <jgotz@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 16:33:48 by jgotz             #+#    #+#             */
-/*   Updated: 2023/11/07 18:52:05 by jgotz            ###   ########.fr       */
+/*   Updated: 2023/11/09 15:58:04 by jgotz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,10 @@
 void	print_usage(void)
 {
 	ft_printf("Usage: ./fractol [mandelbrot | julia]\n");
+	ft_printf("\t-julia\n");
+	ft_printf("\t-julia [ca] [cb]\n");
+	ft_printf("\t-mandelbrot\n");
 	exit(-1);
-}
-
-void	resize(int a, int b, void *param)
-{
-	t_fract	*fract;
-
-	fract = (t_fract *)param;
-	fract->height = b;
-	fract->width = a;
-	mlx_delete_image(fract->mlx, fract->img);
-	fract->img = mlx_new_image(fract->mlx, a, b);
-	if (fract->set == 1)
-		mandelbrot(fract);
-	else if (fract->set == 2)
-		julia(fract);
 }
 
 int	main(int argc, char **argv)
@@ -42,14 +30,28 @@ int	main(int argc, char **argv)
 	if (argc < 2)
 		print_usage();
 	if (!ft_strncmp(argv[1], "mandelbrot", 11))
-		mbt.set = 1;
+	{
+		if (argc == 2)
+			mbt.set = 1;
+		else
+			print_usage();
+	}
 	else if (!ft_strncmp(argv[1], "julia", 6))
 	{
 		mbt.set = 2;
 		if (argc == 4)
 		{
-			mbt.ca = argv[2];
-			mbt.cb = argv[3];
+			mbt.ca = strtodo(argv[2]);
+			mbt.cb = strtodo(argv[3]);
+		}
+		else if (argc == 2)
+		{
+			mbt.ca = -0.8;
+			mbt.cb = 0.156;
+		}
+		else
+		{
+			print_usage();
 		}
 	}
 	else
@@ -59,8 +61,6 @@ int	main(int argc, char **argv)
 	mbt.height = HEIGHT;
 	mbt.offsetx = 0.0;
 	mbt.offsety = 0.0;
-	mbt.ca = -0.8;
-	mbt.cb = 0.156;
 	mlx = mlx_init(WIDTH, HEIGHT, "fract'ol", true);
 	img = mlx_new_image(mlx, WIDTH, HEIGHT);
 	mlx_key_hook(mlx, quit, NULL);
