1bbad25090ad97573e05621caaaa4806f6f2544b
Jonas Götz <jonas.goetz01@web.de>
2023-11-13 12:00:29 +0100

    norminette fix

--- src/fractol.c
+++ src/fractol.c
@@ -6,7 +6,7 @@
 /*   By: jgotz <jgotz@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 16:33:48 by jgotz             #+#    #+#             */
-/*   Updated: 2023/11/12 17:38:01 by jgotz            ###   ########.fr       */
+/*   Updated: 2023/11/13 11:58:29 by jgotz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,12 @@ void	print_usage(void)
 	exit(-1);
 }
 
+void	convert_argv(char **argv, t_fract *mbt)
+{
+	mbt->ca = strtodo(argv[2]);
+	mbt->cb = strtodo(argv[3]);
+}
+
 void	routines(int argc, char **argv, t_fract *mbt)
 {
 	if (!ft_strncmp(argv[1], "mandelbrot", 11))
@@ -35,16 +41,8 @@ void	routines(int argc, char **argv, t_fract *mbt)
 	{
 		mbt->set = 2;
 		if (argc == 4)
-		{
-			mbt->ca = strtodo(argv[2]);
-			mbt->cb = strtodo(argv[3]);
-		}
-		else if (argc == 2)
-		{
-			mbt->ca = -0.8;
-			mbt->cb = 0.156;
-		}
-		else
+			convert_argv(argv, mbt);
+		else if (argc != 2)
 			print_usage();
 	}
 	else if (!ft_strncmp(argv[1], "burning-ship", 13))
@@ -58,6 +56,18 @@ void	routines(int argc, char **argv, t_fract *mbt)
 		print_usage();
 }
 
+void	initialize(t_fract *mbt)
+{
+	mbt->zoom = 2.5;
+	mbt->width = WIDTH;
+	mbt->height = HEIGHT;
+	mbt->offsetx = 0.0;
+	mbt->offsety = 0.0;
+	mbt->mouse_clicked = 0;
+	mbt->ca = -0.8;
+	mbt->cb = 0.156;
+}
+
 int	main(int argc, char **argv)
 {
 	t_fract		mbt;
@@ -66,20 +76,17 @@ int	main(int argc, char **argv)
 
 	if (argc < 2)
 		print_usage();
+	initialize(&mbt);
 	routines(argc, argv, &mbt);
 	img = NULL;
 	mlx = NULL;
-	mbt.zoom = 2.5;
-	mbt.width = WIDTH;
-	mbt.height = HEIGHT;
-	mbt.offsetx = 0.0;
-	mbt.offsety = 0.0;
 	mlx = mlx_init(WIDTH, HEIGHT, "fract'ol", true);
 	img = mlx_new_image(mlx, WIDTH, HEIGHT);
 	mlx_key_hook(mlx, quit, &mbt);
 	mlx_scroll_hook(mlx, scroll, &mbt);
 	mlx_resize_hook(mlx, resize, &mbt);
 	mlx_cursor_hook(mlx, mouse_event, &mbt);
+	mlx_mouse_hook(mlx, mouse_click_event, &mbt);
 	mbt.img = img;
 	mbt.mlx = mlx;
 	scroll(0, 0, &mbt);
