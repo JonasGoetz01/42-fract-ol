e851edb22b7dbfaa9543f9e1421a5df9bc7165fa
Jonas Götz <jonas.goetz01@web.de>
2023-11-13 12:01:03 +0100

    vibrant color function

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: jgotz <jgotz@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 03:19:31 by jgotz             #+#    #+#             */
-/*   Updated: 2023/11/10 13:39:22 by jgotz            ###   ########.fr       */
+/*   Updated: 2023/11/13 11:32:07 by jgotz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,21 @@
 int	get_rgba(int r, int g, int b, int a)
 {
 	return (r << 24 | g << 16 | b << 8 | a);
+}
+
+uint32_t	get_vibrant_rgba(int gradient_value)
+{
+	int	red;
+	int	green;
+	int	blue;
+	int	alpha;
+
+	red = (gradient_value * 2) % 256;
+	green = (gradient_value * 3) % 256;
+	blue = (gradient_value * 4) % 256;
+	red = red / 2;
+	green = green / 2;
+	blue = blue / 2;
+	alpha = 255;
+	return (get_rgba(red, green, blue, alpha));
 }
